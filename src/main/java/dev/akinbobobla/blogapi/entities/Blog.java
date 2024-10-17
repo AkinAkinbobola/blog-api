eabf353354a24c2e13557ee163af8ef400aa0c23
Akin Akinbobola <akinbobola71@gmail.com>
2024-10-17 13:26:47 +0100

    Create schema

--- src/main/java/dev/akinbobobla/blogapi/entities/Blog.java
+++ src/main/java/dev/akinbobobla/blogapi/entities/Blog.java
@@ -1,4 +1,29 @@
 package dev.akinbobobla.blogapi.entities;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class Blog {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String title;
+
+    private String content;
+
+    private String category;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @ElementCollection
+    private List<String> tags;
 }
